8eddc531f25a68e115e7d431e3c6cffabe54a195
Daniel Huigens <d.huigens@protonmail.com>
2025-08-25 20:55:16 +0200

    Update browser test

--- test/browser/download.js
+++ test/browser/download.js
@@ -1,17 +1,20 @@
-var http = require('http');
-var unbzip2Stream = require('../../');
-var concat = require('concat-stream');
-var test = require('tape');
+const unbzip2Stream = require('../../');
+const test = require('tape');
 
-test('http stream piped into unbzip2-stream results in original file content', function(t) {
+test('http stream piped into unbzip2-stream results in original file content', async function(t) {
     t.plan(1);
 
-    http.get({path: '/test/fixtures/text.bz2', responseType: "arraybuffer"}, function(res) {
-        res.pipe( stream.Duplex.fromWeb(unbzip2Stream()) ).pipe(
-            concat(function(data) {
-                var expected = "Hello World!\nHow little you are. now.\n\n";
-                t.equal(data.toString('ascii'), expected);
-            })
-        );
-    });
+    const response = await fetch('/test/fixtures/text.bz2');
+    const responseStream = response.body;
+    const decompressedStream = responseStream
+        .pipeThrough(unbzip2Stream())
+        .pipeThrough(new TextDecoderStream());
+    const chunks = [];
+    for await (const chunk of decompressedStream) {
+        chunks.push(chunk);
+    }
+    const data = chunks.join('');
+
+    const expected = 'Hello World!\nHow little you are. now.\n\n';
+    t.equal(data, expected);
 });
